2f71fa241c43ca8ac729e678797a3157940be0d4
Nikko. Paulo D. Ilar <45785826+nikkoilar@users.noreply.github.com>
2025-10-19 16:24:54 +0800

    Add files via upload

---
+++ user_login.py
@@ -0,0 +1,28 @@
+
+def user_username_test(prompt):
+    while True:
+        try:
+            user_num = str(input(prompt))
+            if user_num == "1234":
+                return user_num
+            else:
+                print("Invalid Input. for username.")
+        except ValueError:
+            print("Invalid Input. Please enter correct username.")
+
+def user_password_test(prompt):
+    while True:
+        try:
+            pass_num = str(input(prompt))
+            if pass_num == "1234":
+                return pass_num
+            else:
+                print("Invalid Input. for password.")
+        except ValueError:
+            print("Invalid Input. Please enter correct password.")
+
+def user_username_input(username):
+    return username
+
+def user_password_input(password):
+    return password
