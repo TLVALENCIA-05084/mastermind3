13c9eb904d958260aff1cfe19b9e0aa267f1c01b
TLVALENCIA-05084 <tlvalencia@alum.up.edu.ph>
2025-10-24 14:19:09 +0800

    Add files via upload

---
+++ game_tj.py
@@ -0,0 +1,17 @@
+import random
+
+def generate_secret_code(colors):
+    colors = ["Red","Green","Blue","Yellow","White","Orange"]
+    secret_code = []
+    for i in range(1,5):
+        secret_code.append(random.choice(colors))
+        break
+    return code
+def right_guess (color):
+    return None
+def right_color_wrong_guess ():
+    return None
+def wrong_guess ():
+    return None
+def main_game ():
+    return None
