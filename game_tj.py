4f242f27a7ce2600a02f5fc13158b88a6add58e5
TLVALENCIA-05084 <tlvalencia@alum.up.edu.ph>
2025-10-25 18:25:01 +0800

    Update game_tj.py

--- game_tj.py
+++ game_tj.py
@@ -1,8 +1,7 @@
 import random
 
 print("""There are six colors that you can use to guess the secret code.\nRed\nGreen\nBlue\nYellow\nWhite\nOrang\nThere are four colors in the secret code.\nCan you guess them all?""")
-colors = ["Red","Green","Blue","Yellow","White","Orange"]
-colors_shorthand = ["R"]
+colors = ["r","g","b","y","w","o"]
 
 def generate_secret_code(colors):
     secret_code = []
@@ -10,7 +9,6 @@ def generate_secret_code(colors):
         secret_code.append(random.choice(colors))
     return secret_code
 def right_guess (colors):
-    color_1 = "Input "
     return None
 def right_color_wrong_guess ():
     return None
